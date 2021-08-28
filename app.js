1ab4dd6ae3174e4fbed0a9658b97b683f8dc5718
MD. Arafin Islam <md.arafinislam1902@gmail.com>
2021-08-28 22:14:54 +0600

    Landing Page Done

---
+++ app.js
@@ -0,0 +1,12 @@
+var typed = new Typed("#lang_type", {
+  strings: [
+    '<i class="js">Javascript</i>',
+    '<i class="vuejs">VueJS</i>',
+    '<i class="django">Django</i>',
+  ],
+  typeSpeed: 30,
+  backSpeed: 30,
+  shuffle: false,
+  smartBackspace: false,
+  loop: true,
+});
