8c4777951cd9019e00a241ddff68990947d87c4d
MD. Arafin Islam <md.arafinislam1902@gmail.com>
2021-08-30 16:33:56 +0600

    projects screen added

--- app.js
+++ app.js
@@ -9,5 +9,29 @@ var typed = new Typed("#lang_type", {
   shuffle: false,
   smartBackspace: false,
   loop: true,
-  cursorChar: '_',
+  cursorChar: "_",
 });
+
+// PROJECTS LOOP
+
+
+function projectloop(data) {
+  var project = "";
+  for (var i = 0; i < data.length; i++) {
+    var projectdiv = `<div class="bg-gray-50 w-11/12 p-3 border-2 rounded-lg shadow-lg text-center mb-8">
+    <div class="text-xl text-purple-600 font-medium">${data[i].name}</div>
+    <div class="px-4">
+      ${data[i].description}
+    </div>
+    <div class="flex items-center justify-between">
+      <a href="https://www.github.com/arafin02/${data[i].github}"><img class="icon" src="/media/github.svg" alt=""></a>
+      <a class="text-purple-500" href="${data[i].url}">View Site</a>
+    </div>
+  </div>`;
+    project += projectdiv
+  }
+  document.getElementById('projects').innerHTML = project
+
+}
+
+projectloop(projects)
---
+++ projects.js
@@ -0,0 +1,38 @@
+const projects = [
+  {
+    name: 'Semantic CSS',
+    url: 'https://www.tailwindcss.com',
+    github: 'arafin02',  
+    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
+  },
+  {
+    name: 'Tailwind CSS',
+    url: 'https://www.tailwindcss.com',
+    github: 'arafin02',
+    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
+  },
+  {
+    name: 'Materialize CSS',
+    url: 'https://www.tailwindcss.com',
+    github: 'arafin02',
+    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
+  },
+  {
+    name: 'Bootstrap CSS',
+    url: 'https://www.tailwindcss.com',
+    github: 'arafin02',
+    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
+  },
+  {
+    name: 'Foundation CSS',
+    url: 'https://www.tailwindcss.com',
+    github: 'arafin02',
+    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
+  },
+  {
+    name: 'Vuetify CSS',
+    url: 'https://www.tailwindcss.com',
+    github: 'arafin02',
+    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
+  },
+]
