fe29acdce2b5a0993d8a3b89ff6fda58fca9b861
MD. Arafin Islam <md.arafinislam1902@gmail.com>
2021-08-30 21:52:57 +0600

    version 1.0.0 is done

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ var typed = new Typed("#lang_type", {
 function projectloop(data) {
   var project = "";
   for (var i = 0; i < data.length; i++) {
-    var projectdiv = `<div class="bg-gray-50 w-11/12 p-3 border-2 rounded-lg shadow-lg text-center mb-8">
-    <div class="text-xl text-purple-600 font-medium">${data[i].name}</div>
+    var projectdiv = `<div class="bg-gray-50 w-11/12 p-3 border-2 rounded-lg shadow-lg text-center mb-8 hover:bg-purple-100">
+    <div class="text-xl text-purple-600 hover:text-purple-800 font-medium">${data[i].name}</div>
     <div class="px-4">
       ${data[i].description}
     </div>
     <div class="flex items-center justify-between">
       <a href="https://www.github.com/arafin02/${data[i].github}"><img class="icon" src="/media/github.svg" alt=""></a>
-      <a class="text-purple-500" href="${data[i].url}">View Site</a>
+      <a class="text-purple-500 hover:text-purple-700" href="${data[i].url}">View Site</a>
     </div>
   </div>`;
     project += projectdiv
--- projects.js
+++ projects.js
@@ -1,38 +1,8 @@
 const projects = [
   {
-    name: 'Semantic CSS',
-    url: 'https://www.tailwindcss.com',
-    github: 'arafin02',  
-    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
-  },
-  {
-    name: 'Tailwind CSS',
-    url: 'https://www.tailwindcss.com',
-    github: 'arafin02',
-    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
-  },
-  {
-    name: 'Materialize CSS',
-    url: 'https://www.tailwindcss.com',
-    github: 'arafin02',
-    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
-  },
-  {
-    name: 'Bootstrap CSS',
-    url: 'https://www.tailwindcss.com',
-    github: 'arafin02',
-    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
-  },
-  {
-    name: 'Foundation CSS',
-    url: 'https://www.tailwindcss.com',
-    github: 'arafin02',
-    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
-  },
-  {
-    name: 'Vuetify CSS',
-    url: 'https://www.tailwindcss.com',
-    github: 'arafin02',
-    description: 'The official Tailwind CSS IntelliSense extension for Visual Studio Code enhances the Tailwind development experience.',
+    name: 'FC Pink Poison',
+    url: 'https://www.fcpp.netlify.app',
+    github: 'fcp',  
+    description: 'A minimalistic web application for Pink Poison Football Club',
   },
 ]
