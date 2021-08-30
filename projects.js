0aa26807cb82d69999f0fe49bdb1ebfaf754e42b
Md. Arafin Islam <65769892+arafin02@users.noreply.github.com>
2021-08-30 22:05:05 +0600

    Fcpp site url added

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@ const projects = [
   {
     name: 'FC Pink Poison',
     url: 'https://www.fcpp.netlify.app',
-    github: 'fcp',  
+    github: 'pinkpoison',  
     description: 'A minimalistic web application for Pink Poison Football Club',
   },
 ]
